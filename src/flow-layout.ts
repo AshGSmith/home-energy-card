19383b2351b62d6f287211924fc7fd20a044bc8a
Ash Smith <ash@cresthouse.net>
2026-04-23 12:25:45 +0100

    chore: prepare v0.2.0-beta.06

--- src/flow-layout.ts
+++ src/flow-layout.ts
@@ -86,7 +86,7 @@ export class HecFlowLayout extends LitElement {
     .slot-ev      {
       grid-column: 3;
       grid-row: 1;
-      transform: translate(-44px, 0px);
+      transform: translate(-49px, 0px);
     }  /* softened away from rigid C1 alignment */
     .slot-grid    { grid-column: 1; grid-row: 2; }  /* A2 */
     .slot-home    { grid-column: 2; grid-row: 2; }  /* B2 */
